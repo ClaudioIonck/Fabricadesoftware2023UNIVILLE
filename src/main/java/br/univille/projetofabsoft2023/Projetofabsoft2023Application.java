2293aeedc581f156c9d26e08541b1c9ea23ed008
CLAUDIO LUIZ DEFREIN IONCK <claudio.ionck@univille.br>
2023-03-24 20:27:35 -0300

    add project

---
+++ src/main/java/br/univille/projetofabsoft2023/Projetofabsoft2023Application.java
@@ -0,0 +1,13 @@
+package br.univille.projetofabsoft2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetofabsoft2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetofabsoft2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/projetofabsoft2023/Projetofabsoft2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projetofabsoft2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetofabsoft2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
