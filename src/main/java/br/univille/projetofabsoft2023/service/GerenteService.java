95f425482b3ba0f4dbd7376b253468c658b8fce8
CLAUDIO LUIZ DEFREIN IONCK <claudio.ionck@univille.br>
2023-04-03 22:24:15 -0300

    Add Gerente no DB

---
+++ src/main/java/br/univille/projetofabsoft2023/service/GerenteService.java
@@ -0,0 +1,8 @@
+package br.univille.projetofabsoft2023.service;
+
+import java.util.List;
+import br.univille.projetofabsoft2023.entity.Gerente;
+
+public interface GerenteService {
+    List<Gerente> getAll();
+}
