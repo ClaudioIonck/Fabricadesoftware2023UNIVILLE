e692fe71a67785b4389f1f464dc3e2b1722c59bf
Claudio Luiz Defrein Ionck <78597253+ClaudioIonck@users.noreply.github.com>
2023-05-20 15:36:19 -0300

    Gravar

--- src/main/java/br/univille/projetofabsoft2023/controller/User/ClienteController.java
+++ src/main/java/br/univille/projetofabsoft2023/controller/User/ClienteController.java
@@ -3,9 +3,11 @@ package br.univille.projetofabsoft2023.controller.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.univille.projetofabsoft2023.entity.User.Cliente;
 import br.univille.projetofabsoft2023.service.ClienteService;
 
 @Controller
@@ -21,6 +23,23 @@ public class ClienteController {
         var listaCliente = clienteService.getAll();
 
         return new ModelAndView("user/cliente/index", "listaClientes", listaCliente);
+    }
+
+    @GetMapping("/novo")
+    public ModelAndView novo() {
+
+        var cliente = new Cliente();
+
+        return new ModelAndView("user/cliente/form", "cliente", cliente);
+
+    }
+
+    @PostMapping(params = "form")
+    public ModelAndView save(Cliente cliente) {
+
+        clienteService.save(cliente);
+
+        return new ModelAndView("redirect:/cliente/");
     }
 
 }
