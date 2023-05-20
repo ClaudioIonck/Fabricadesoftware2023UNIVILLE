c0984eded898240624076a3b9604f03438c6f096
Claudio Luiz Defrein Ionck <78597253+ClaudioIonck@users.noreply.github.com>
2023-05-20 16:29:13 -0300

    GravarGerente

--- src/main/java/br/univille/projetofabsoft2023/controller/User/GerenteController.java
+++ src/main/java/br/univille/projetofabsoft2023/controller/User/GerenteController.java
@@ -3,9 +3,11 @@ package br.univille.projetofabsoft2023.controller.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.univille.projetofabsoft2023.entity.User.Gerente;
 import br.univille.projetofabsoft2023.service.GerenteService;
 
 @Controller
@@ -20,5 +22,21 @@ public class GerenteController {
         var listaGerente = gerenteService.getAll();
 
         return new ModelAndView("User/Gerente/index", "listaGerente", listaGerente);
+    }
+
+    @GetMapping("/novo")
+    public ModelAndView novo() {
+
+        var gerente = new Gerente();
+
+        return new ModelAndView("user/gerente/form", "gerente", gerente);
+    }
+
+    @PostMapping(params = "form")
+    public ModelAndView save(Gerente gerente) {
+
+        gerenteService.save(gerente);
+
+        return new ModelAndView("redirect:/gerente/");
     }
 }
