e2ef63c8aae080871ccc29037967700c5636f497
Claudio Luiz Defrein Ionck <78597253+ClaudioIonck@users.noreply.github.com>
2023-05-26 20:18:52 -0300

    Login

--- src/main/java/br/univille/projetofabsoft2023/controller/User/ClienteController.java
+++ src/main/java/br/univille/projetofabsoft2023/controller/User/ClienteController.java
@@ -1,6 +1,7 @@
 package br.univille.projetofabsoft2023.controller.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import br.univille.projetofabsoft2023.service.ClienteService;
 
 @Controller
 @RequestMapping("/cliente")
+@PreAuthorize("hasAuthority('APPROLE_admin')")
 public class ClienteController {
 
     @Autowired
